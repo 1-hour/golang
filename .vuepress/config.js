640c01532989342f8a95528baecc4b4dae5e790c
jiusanzhou <jsz3@live.com>
2019-08-18 00:09:40 +0800

    :rocket: INit documents

---
+++ .vuepress/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    base: '/golang/',
+    title: 'Golang・1小时指南',
+    description: '每天1小时，从零开始学习Go',
+    head: [['link', { rel: 'icon', type: 'image/png', href: '/favicon.ico' }]],
+}
