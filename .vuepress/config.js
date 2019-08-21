dcbdd8d78fb7a13ae031126807518e9a4194e9a2
Zoe <jsz3@live.com>
2019-08-21 14:02:50 +0100

    :tada: init the basic layout

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,6 +1,43 @@
 module.exports = {
-    base: '/golang/',
-    title: 'Golang・1小时指南',
-    description: '每天1小时，从零开始学习Go',
-    head: [['link', { rel: 'icon', type: 'image/png', href: '/favicon.ico' }]],
-}
+  base: "/golang/",
+  title: "Golang・1小时指南",
+  description: "每天1小时，从零开始学习Go",
+  head: [["link", { rel: "icon", type: "image/png", href: "/favicon.ico" }]],
+  themeConfig: {
+    repo: "1-hour/golang",
+    repoLabel: "在GitHub上编辑",
+    editLinkText: "在GitHub上编辑此页",
+    editLinks: true,
+    sidebar: [
+      {
+        title: "关于",
+        collapsable: false,
+        children: [
+          "/about/",
+        ]
+      },
+      {
+        title: "入门指南",
+        collapsable: false,
+        children: [
+          "/guide/",
+          "/guide/getting-started-with-go/"
+        ]
+      },
+      {
+        title: "高级教程",
+        collapsable: false,
+        children: [
+            "/effictive/",
+        ]
+      },
+      {
+        title: "项目实战",
+        collapsable: false,
+        children: [
+            "/real-world/"
+        ]
+      }
+    ]
+  }
+};
---
+++ about/README.md
@@ -0,0 +1,18 @@
+# 关于
+
+[**Golang・1小时指南**](https://1hour.guide/golang)项目是属于由[周筱鲁](https://zoe.im)发起的[**1小时指南**](https://1hour.guide)的子项目。
+
+[**Golang・1小时指南**](https://1hour.guide/golang)希望带领大家从零去
+
+## 我们是谁
+
+本项目暂时由个人发起，目前参与贡献的有：
+- [周筱鲁](https://zoe.im)
+
+
+
+## 1小时指南
+
+想了解更多有关[**1小时指南**](https://1hour.guide)的信息可以查看[**1小时指南**](https://1hour.guide)项目的主页: [https://1hour.guide](https://1hour.guide)。有任何建议或意见欢迎通过邮箱[hi@1hour.guide](mailto:hi@1hour.guide)与我们取得联系，或在项目上[创建issue](https://github.com/1-hour/golang/issues/new)。
+
+
